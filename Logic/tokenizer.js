7498ebd33d3e129bb282d1a192daf743e56b341b
DiDev <didev4207@gmail.com>
2025-08-04 13:16:14 -0400

    Add JWT to handle user session

---
+++ Logic/tokenizer.js
@@ -0,0 +1,46 @@
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import { KEY } from "../db/secret.js";
+
+// expects object type like user.name, user.role
+function genToken(user) {
+  try {
+    if (!user.id || !user.name || !user.role) {
+      console.error("User fields missing to generate token");
+      return;
+    }
+    const token = jwt.sign(user, KEY, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    });
+    return token;
+  } catch (error) {
+    throw new JsonWebTokenError(
+      `Exception made trying to generate token, ${error}`
+    );
+  }
+}
+
+function decodeToken(payload) {
+  try {
+    if (!payload) {
+      console.error("Payload missing");
+      return;
+    }
+
+    jwt.verify(
+      payload,
+      KEY,
+      {
+        algorithm: "HS256",
+      },
+      (err, decode) => {
+        if (err) throw err;
+        return decode;
+      }
+    );
+  } catch (error) {
+    throw new JsonWebTokenError(
+      `Exception made trying to decode payload: ${error}`
+    );
+  }
+}
