9f70c157aca42161bbf114ffcd61bea0c25f9eb5
DiDev <didev4207@gmail.com>
2025-08-06 17:44:45 -0400

    Complete backend & progress dashboard frontend development

---
+++ db/connection.js
@@ -0,0 +1,342 @@
+import { HOST, PASSWORD, USER, DATABASE, PORT } from "./secret.js";
+import mysql from "mysql";
+
+let conn = null;
+
+async function connectToDb() {
+  try {
+    conn = mysql.createConnection({
+      host: HOST,
+      port: PORT,
+      user: USER,
+      password: PASSWORD,
+      database: DATABASE,
+    });
+
+    conn.connect((err) => {
+      if (err) {
+        if (err.code == "ECONNRESET")
+          console.error(
+            "Try and check if proxy or vpn is active, if so try again without it."
+          );
+        else if(err.code == "PROTOCOL_CONNECTION_LOST")
+          console.error("Lost connection to db, try reconnecting again")
+        throw err;
+      }
+      console.log("Database connected");
+    });
+  } catch (error) {
+    console.error(`Expection while trying to connect to db: ${error}`);
+  }
+}
+
+export function comparePassword(password, password_hash) {
+  bcrypt.compare(password, password_hash, (err, result) => {
+    if (err) throw err;
+    return result;
+  });
+}
+
+export async function authtenticateUser(name = "", email = "", password = "") {
+  try {
+    if (!conn) await connectToDb();
+    if (!(name || email) || !password) {
+      console.error("Fields missing to authenticate user");
+      return;
+    }
+    let passwordHash = "";
+    conn.query(
+      "SELECT * FROM users WHERE username=? OR email=?",
+      (err, result) => {
+        if (err) throw err;
+        passwordHash = result[0]?.password_hash;
+      }
+    );
+
+    if (!passwordHash) return false;
+    return true ? comparePassword(password, passwordHash) : false;
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export async function getUsers() {
+  try {
+    if (!conn) await connectToDb();
+    return new Promise((resolve, reject) => {
+      conn.query("CALL get_users()", (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export async function getAccounts() {
+  try {
+    if (!conn) console.log("Couldn't find connection to MySQL.");
+    return new Promise((resolve, reject) => {
+      conn.query("CALL get_accounts()", (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export async function getTransactions() {
+  try {
+    if (!conn) console.log("Couldn't find connection to MySQL.");
+    return new Promise((resolve, reject) => {
+      conn.query("CALL get_transactions()", (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  } catch (error) {}
+}
+
+async function getPasswordHash(name = "", email = "") {
+  try {
+    if (!conn) await connectToDb();
+    if (!name && !email) {
+      console.error(
+        "Can not get password from user without either its username or email"
+      );
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      conn.query(
+        "SELECT password_hash FROM users WHERE username=? OR email=?",
+        [name, email],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result[0]?.password_hash);
+        }
+      );
+    });
+  } catch (error) {
+    if (error.message == "Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+  }
+}
+
+export async function getUserId(name = "", email = "") {
+  try {
+    if (!conn) await connectToDb();
+    if (!(name && email)) {
+      console.error("User fields missing to get id");
+      return;
+    }
+    const passwordHash = await getPasswordHash(name, email);
+    if (!passwordHash) return -1;
+
+    return new Promise((resolve, reject) => {
+      conn.query(
+        "SELECT id FROM users WHERE username=? OR email=? AND password_hash=?",
+        [name, email, passwordHash],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result[0]?.id);
+        }
+      );
+    });
+  } catch (error) {
+    if (error.message == "Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+  }
+}
+
+export function addNewUser(name, email, password, role = "user") {
+  try {
+    if (!conn) {
+      connectToDb();
+    }
+
+    if (!name || !email || !password || !role)
+      console.error("User fields missing");
+    conn.query(
+      "CALL insert_user(?, ?, ?, ?)",
+      [name, email, password, role],
+      (err, result) => {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  } catch (error) {
+    if (error.message == "Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+  }
+}
+
+export function addNewTransaction(
+  description,
+  amount,
+  accountId,
+  state = "On Queque"
+) {
+  try {
+    if (!description || !amount || !accountId || !state)
+      console.error("Transactions fields missing");
+    conn.query(
+      "CALL insert_transaction (?, ?, ?, ?)",
+      [description, amount, accountId],
+      (err, result) => {
+        if (err) throw err;
+        return result;
+      }
+    );
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export function addNewAccount(uid, amount, type) {
+  try {
+    if (!uid || !amount || !type) console.error("User fields missing");
+    conn.query(
+      "CALL insert_account(?, ?, ?)",
+      [uid, amount, type],
+      (err, result) => {
+        if (err) throw err;
+        return result;
+      }
+    );
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export function deleteUser(id) {
+  try {
+    if (!conn) connectToDb();
+    if (!id) console.error("User.id missing");
+    conn.query("CALL delete_user(?)", [id], (err, result) => {
+      if (err) throw err;
+      return result;
+    });
+  } catch (error) {
+    if (error.message == "Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+  }
+}
+
+export function deleteTransaction(id) {
+  try {
+    if (!id) console.error("Transactions id missing");
+    conn.query("CALL delete_transaction(?)", [id], (err, result) => {
+      if (err) throw err;
+      return result;
+    });
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export function deleteAccount(id) {
+  try {
+    if (!id) console.error("Account id missing");
+    conn.query("CALL delete_account(?)", [id], (err, result) => {
+      if (err) throw err;
+      return result;
+    });
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export function updateUser(id, name, email, password, role = "user") {
+  if (!id || !name || !email || !password || !role)
+    console.error("User fields missing");
+  conn.query(
+    "CALL actualizar_usuario (?, ?, ?, ?, ?)",
+    [id, name, email, password, role],
+    (err, result) => {
+      if (err) throw err;
+      return result;
+    }
+  );
+}
+
+export function updateTransaction(id, description, amount, state) {
+  try {
+    if (!id || !description || !amount || !state)
+      console.error("Transactions fields missing");
+    conn.query(
+      "CALL update_transaction (?, ?, ?, ?)",
+      [id, description, amount, state],
+      (err, result) => {
+        if (err) throw err;
+        return result;
+      }
+    );
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
+
+export function updateAccount(id, uid, amount, type) {
+  try {
+    if (!id || !uid || !amount || !type)
+      console.error("Account fields missing");
+    conn.query(
+      "CALL update_account(?, ?, ?, ?)",
+      [id, uid, amount, type],
+      (err, result) => {
+        if (err) throw err;
+        return result;
+      }
+    );
+  } catch (error) {
+    if (error == "ReferenceError: Cannot access 'conn' before initialization") {
+      console.error("DB not connected");
+      return;
+    }
+    throw error;
+  }
+}
---
+++ db/secret.js
@@ -0,0 +1,7 @@
+// CONSTANT VARIABLES
+export const HOST="b2mwceotbbd62y6zhwst-mysql.services.clever-cloud.com";
+export const PORT=3306;
+export const USER="umupvtwhvup87dlp";
+export const DATABASE="b2mwceotbbd62y6zhwst";
+export const PASSWORD="I3ygOUPvHBLxH2ZqbN5d";
+export const KEY="0000";
